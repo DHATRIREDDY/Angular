7533d01de297bbdf0483609d146b1516d7fb9925
DHATRIREDDY <dhatrireddy007@gmail.com>
2019-06-07 10:56:22 -0700

    ufirst

--- src/app/pcomp/pcomp.component.ts
+++ src/app/pcomp/pcomp.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pcomp',
-  templateUrl: '<h2> welcome {{name}}</h2> To world cup<h2>',
-  styleUrls: ['.ps{color:blue;} .pr{color:pink;} .pt{color:ALGERIAN;}']
+  template: '<h2> welcome {{name}}</h2><h2 class="ps"> To world cup</h2><h2 [class]="sclass">To world cup</h2><h2  [class.pr]="error">To world cup</h2><h2 [ngClass]="mssg">To world cup</h2>',
+  styles: ['.ps{color:blue;} .pr{color:brown;} .pt{font-style:italic;}']
 })
 export class PcompComponent implements OnInit {
   public name="Aditi";
-
+  public sclass="ps";
+  public error="false";
+  public sps="true";
+  public mssg={
+    "ps":!this.error,
+    "pr":this.error,
+    "pt":this.sps
+  }
   constructor() { }
 
   ngOnInit() {
